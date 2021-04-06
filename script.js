d032b62ac698408b6e4994700a08728bfee7c9bf
Matthew <mattj20@gmail.com>
2021-04-06 15:16:37 -0700

    Updated background and added flavor text

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript"; 
-
-document.querySelector('body').appendChild(h2); 
+const h1 = document.createElement('h1');
+h1.textContent = "Let's Pet Some Cats!"; 
+document.querySelector('body').appendChild(h1); 

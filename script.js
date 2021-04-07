3147b231c8281b21deb81d43b307f35a9f27cadf
Matthew <mattj20@gmail.com>
2021-04-07 13:04:34 -0700

    Added purring to cats

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const h1 = document.createElement('h1');
 h1.textContent = "Let's Pet Some Cats!"; 
 document.querySelector('body').appendChild(h1); 
+
+const catPurr = document.getElementById("catty");
+
+catPurr.addEventListener("click", playPurr);
+
+const catsPurr = document.getElementsByClassName("catImages")
+
+for (let i = 0; i < catsPurr.length; i++) {
+    catsPurr[i].addEventListener("click",playPurr);
+}
+
+function playPurr(){
+    let audio = new Audio("cat_purr.mp3");
+    audio.play();
+}
